ec8b50e1e0f5f55ca55dd72402022f7e33c549d0
pepato <fra_enrico@yahoo.it>
2015-12-11 19:25:36 +0100

    initial commit
    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # tes-books
+
+This little project aims to scrape The Imperial Library [1] to assemble all the books from The Elder Scrolls IV: Oblivion into a single epub.
+
+The code is just a Python script which uses BeautifulSoup to edit the files. The files will be then passed to Pandoc, which will create the epub using the following command:
+
+	pandoc  -S -o oblivion-books.epub --epub-metadata=title.txt --epub-cover-image=images/ob-cover.jpg  --epub-chapter-level=2 content/*.html -f html-native_divs
+
+The script is based on a Directory system which goes like this:
+
+by-title-oblivion.html		list of books from Oblivion
+by-title-skyrim.html		list of books from Skyrim
+./content/			the html source files for each book
+./out/				the output directory
+./out/content/			where the edited files go
+./out/images/			where images are stored (i.e. the book cover)
+./out/title.txt			the yaml metadata file for Pandoc
+
+[1] http://imperiallibrary.info
---
+++ zuppa-rec.py
@@ -0,0 +1,75 @@
+#!/usr/local/bin/python
+# coding: utf-8
+from __future__ import print_function # to write to file with Python 3 syntax - this needs to go on top
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+
+# let's open our index-list file
+# soup = BeautifulSoup(open("by-title-skyrim.html")) #remeber to use the right index file according to the books you want to import
+soup = BeautifulSoup(open("by-title-oblivion.html")) #remeber to use the right index file according to the books you want to import
+
+ldir = []
+for link in soup.find(id='content').find_all("a"):
+	# print(link.get("href")) # test
+	ldir.append(link.get("href"))
+
+# print(ldir) #test
+
+# function to read the file, clean it up and create the prettified output
+def zupparic(file_book):
+	# open the file to edit
+	source = open("./"+file_book+".html")
+
+	# telling SoupStrainer what I want from the source html
+	main = SoupStrainer(id='main')
+
+	# making the soup
+	content = BeautifulSoup(source, "html.parser", parse_only=main) #need to specify the parser cause html5 doesn't work with SoupStrainer
+
+	# cleaning away with decompose the stuff I don't need
+	content.find("div", id="main").unwrap() #stripping away the container tag, cause the epub chapter must begin with h1
+
+	# since not all files are equal, I must handle exceptions
+	try:
+		content.find("div", class_="breadcrumb").decompose()
+	except AttributeError:
+		pass
+	try:
+		content.find("div", class_="node-submitted").decompose()
+	except AttributeError:
+		pass
+	try:
+		content.find("div", class_="field-field-comment").decompose()
+	except AttributeError:
+		pass
+	try:
+		content.find("div", class_="book-navigation").decompose()
+	except AttributeError:
+		pass
+	try:
+		content.find("div", class_="terms terms-inline").decompose()
+	except AttributeError:
+		pass
+	try:
+		content.find("img").decompose()
+	except AttributeError:
+		pass
+
+	# prettify everything
+	final = content.prettify("utf-8")
+
+	# writing the file
+	# watch out the directories of input and output, make sure the paths are correct: uri names must be built right
+	file_out = "./out"+str(file_book)+".html" #renaming files
+	pretty = open(file_out,"w")
+	print(final, file=pretty)
+	pretty.close()
+	# maybe you can do this with less lines, but whatever
+	# now the /out folder should contain all the books ready to be imported. You will need to delete *manually* the ones you don't want
+
+# zupparic("gods-and-worship.html") # test
+
+# running the function ricoursively on each file
+for book in ldir:
+	zupparic(book)
+
